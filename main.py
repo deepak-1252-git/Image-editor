071db50b3b4cef1acd676396798708bcfb557070
Deepak <db5608174@gmail.com>
2026-03-31 11:54:02 +0530

    fixed last

--- main.py
+++ main.py
@@ -214,5 +214,9 @@ def get_output_file(filename):
 def download_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename, as_attachment=True)
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
